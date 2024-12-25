9e89fc8252c1bce4f11e78c526d56172b4cff101
JetsadaWijit <jetsadawijit@outlook.com>
2024-12-25 02:36:04 +0000

    Fix the function declaration

--- mcengine-currency-api/src/main/java/io/github/mcengine/api/database/MCEngineCurrencyApiDBInterface.java
+++ mcengine-currency-api/src/main/java/io/github/mcengine/api/database/MCEngineCurrencyApiDBInterface.java
@@ -1,13 +1,13 @@
 package io.github.mcengine.api.database;
 
 public interface MCEngineCurrencyApiDBInterface {
-    public void connect();
-    public void createTable();
-    public void disConnection();
-    public double getCoin(String playerUuid, String coinType);
-    public Connection getConnection();
-    public void insertCurrency(String playerUuid, double coin, double copper, double silver, double gold);
-    public void insertTransaction(String playerUuidSender, String playerUuidReceiver, String currencyType, String transactionType, double amount, String notes);
-    public boolean playerExists(String uuid);
-    public void updateCurrencyValue(String playerUuid, String operator, String coinType, double amt);
+    void connect();
+    void createTable();
+    void disConnection();
+    double getCoin(String playerUuid, String coinType);
+    Connection getConnection();
+    void insertCurrency(String playerUuid, double coin, double copper, double silver, double gold);
+    void insertTransaction(String playerUuidSender, String playerUuidReceiver, String currencyType, String transactionType, double amount, String notes);
+    boolean playerExists(String uuid);
+    void updateCurrencyValue(String playerUuid, String operator, String coinType, double amt);
 }
